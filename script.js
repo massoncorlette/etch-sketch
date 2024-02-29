fa50e21781b0f2f5a454b2bb3bafe9a93e747b5e
massoncorlette <massoncorlette07@gmail.com>
2024-02-29 05:22:40 -0500

    initial layout

---
+++ script.js
@@ -0,0 +1,12 @@
+
+// Giving the function argument a default value 16 if there is no provided argument
+function gridFlex (size = 16) {
+  let gridArray = [size];
+
+  gridArray.forEach((item) => {
+
+
+    
+  })
+
+}
