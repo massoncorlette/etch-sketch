5f66407f3b32fae70d316f760ff0a9b391b9a635
massoncorlette <massoncorlette07@gmail.com>
2024-03-01 02:54:53 -0500

    added div grid using javascript and flexbox

--- script.js
+++ script.js
@@ -1,12 +1,20 @@
 
 // Giving the function argument a default value 16 if there is no provided argument
 function gridFlex (size = 16) {
-  let gridArray = [size];
+  const gridDiv = document.querySelector('.screen')
+  // using nested loop, on every instance of row made, inner loop appends 16 cells
+  for (let i = 0; i < 16; i++) {
+    const row = document.createElement('div');
+    row.classList.add('grid');
+    gridDiv.appendChild(row);
 
-  gridArray.forEach((item) => {
-
-
-    
-  })
+    for (let j = 0; j < 16; j++) {
+      const cell = document.createElement('div');
+      cell.classList.add('.gridRows');
+      row.appendChild(cell);
+    }
+  }
 
 }
+
+gridFlex();
